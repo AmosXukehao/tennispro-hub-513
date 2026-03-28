3a15ee9773f141db4a7fbd55c0b17059b44582bd
AmosXukehao <1959553737@qq.com>
2026-03-28 11:31:42 +0800

    Delete page-forum.php

--- page-forum.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Page for slug 'forum' — uses Discussion Forum (Capstone) template.
- */
-defined( 'ABSPATH' ) || exit;
-
-require get_stylesheet_directory() . '/template-forum.php';
-
