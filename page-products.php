56346c93cd63031d97e1177da5c5cab9e603f977
AmosXukehao <1959553737@qq.com>
2026-03-28 11:32:22 +0800

    Delete page-products.php

--- page-products.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Page for slug 'products' — uses Products (Capstone) template.
- * This allows using the custom layout without selecting a template in the editor.
- */
-defined( 'ABSPATH' ) || exit;
-
-require get_stylesheet_directory() . '/template-products.php';
-
