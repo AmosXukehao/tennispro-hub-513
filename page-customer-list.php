089fb91645c7f912046e7ef3c1fb4bfbfb81eb4e
AmosXukehao <1959553737@qq.com>
2026-03-28 11:32:31 +0800

    Delete page-customer-list.php

--- page-customer-list.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Page for slug 'customer-list' — uses Customer List (Capstone) template.
- */
-defined( 'ABSPATH' ) || exit;
-
-require get_stylesheet_directory() . '/template-customer-list.php';
-
