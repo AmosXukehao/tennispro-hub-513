d59bda080d419284fad8b8fcd8c9ae2ba574afb6
AmosXukehao <1959553737@qq.com>
2026-03-28 11:29:21 +0800

    Delete page-support.php

--- page-support.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Page for slug 'support' — uses Customer Support (Capstone) template.
- */
-defined( 'ABSPATH' ) || exit;
-
-require get_stylesheet_directory() . '/template-support.php';
-
