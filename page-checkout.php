8cf863d4f79881ffcc8367c8e8855f28d395b4a3
AmosXukehao <1959553737@qq.com>
2026-03-28 11:32:11 +0800

    Delete page-checkout.php

--- page-checkout.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Page for slug 'checkout' — uses Checkout Login (Capstone) template.
- */
-defined( 'ABSPATH' ) || exit;
-
-require get_stylesheet_directory() . '/template-checkout.php';
-
