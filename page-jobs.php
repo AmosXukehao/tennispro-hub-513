64b6f4e66f3d1d64db369053789dcef136f8cce1
AmosXukehao <1959553737@qq.com>
2026-03-28 11:32:47 +0800

    Delete page-jobs.php

--- page-jobs.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Page for slug 'jobs' — uses Recruitment / Careers (Capstone) template.
- */
-defined( 'ABSPATH' ) || exit;
-
-require get_stylesheet_directory() . '/template-jobs.php';
-
